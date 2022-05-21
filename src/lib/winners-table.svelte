8e9ea69d31f88dc2bc7525b195a4f96f0ff2d5c7
r8mkax <64227216+r8mkax@users.noreply.github.com>
2022-05-21 12:15:12 +0300

    Update winners-table.svelte

--- src/lib/winners-table.svelte
+++ src/lib/winners-table.svelte
@@ -52,7 +52,7 @@ export let results = [];
       <br />
     {/each}
     <br />
-    Total profit (counting all binance BNB fees) {w.profit.sub( 1 ).mul( 100 ).toFixed( 4 )}%
+    Total profit (counting all binance BNB fees) {w.profit.sub( 1 ).mul( 44 ).toFixed( 4 )}%
     <br />
   </p>
   <hr/>
